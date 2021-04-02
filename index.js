b9272cd7fff2b58b6256e36a672f635ea0b3bcd8
tuanlong1202 <tuanlong1202@gmail.com>
2021-04-02 19:49:23 -0400

    Phase 1 js basics strings code along

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Susan Smith";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+let shortGreeting = `Welcome, ${currentUser.slice(0,1).toUpperCase()}!`;
